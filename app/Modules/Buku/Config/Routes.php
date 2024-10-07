ede2ed5c9f1259f03238630d0443297c549e0ae1
Reinharrd <83526674+Reinharrd@users.noreply.github.com>
2024-10-07 09:45:55 +0700

    Routes Buku

--- app/Modules/Buku/Config/Routes.php
+++ app/Modules/Buku/Config/Routes.php
@@ -9,10 +9,16 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-$routes->group(
-    'data-buku',
-    ['namespace' => '\Modules\Buku\Controllers'],
-    function ($routes) {
-        $routes->get('/', 'Buku::data_buku');
-    }
-);
+// $routes->group(
+//     'data-buku',
+//     ['namespace' => '\Modules\Buku\Controllers'],
+//     function ($routes) {
+//         $routes->get('/', 'Buku::data_buku');
+//     }
+// );
+$routes->group('api', ['namespace' => '\Modules\Buku\Controllers'], function($routes) {
+    $routes->get('buku', 'Buku::index');
+    $routes->post('buku', 'Buku::create');
+    $routes->put('buku/(:segment)', 'Buku::update/$1');
+    $routes->delete('buku/(:segment)', 'Buku::delete/$1');
+});
