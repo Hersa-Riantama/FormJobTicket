c7d13872c782662a97e4037bd131f2d2f22ff521
Hersa-Riantama <hersarb28@gmail.com>
2024-10-08 09:29:04 +0700

    Update

--- app/Modules/Auth/Views/login.php
+++ app/Modules/Auth/Views/login.php
@@ -188,25 +188,6 @@
                 , made with ❤️ by
                 <a href="https://erlanggaonline.com/" target="_blank" class="footer-link fw-medium">ErlanggaOnline</a>
             </div>
-
-            <!-- <div class="d-none d-lg-inline-block">
-                <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-                  <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
-
-                  <a
-                    href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/"
-                    target="_blank"
-                    class="footer-link me-4"
-                    >Documentation</a
-                  >
-
-                  <a
-                    href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
-                    target="_blank"
-                    class="footer-link"
-                    >Support</a
-                  >
-            </div> -->
         </div>
     </footer>
     <!-- / Footer -->
