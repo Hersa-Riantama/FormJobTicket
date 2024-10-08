49bdd273530bb9871e1fa3297ef3a60172cd746d
Reinharrd <83526674+Reinharrd@users.noreply.github.com>
2024-10-08 12:18:12 +0700

    edo

--- app/Modules/Buku/Config/Routes.php
+++ app/Modules/Buku/Config/Routes.php
@@ -18,7 +18,7 @@ $routes->get('/', 'Home::index');
 // );
 $routes->group('api', ['namespace' => '\Modules\Buku\Controllers'], function($routes) {
     $routes->get('buku', 'Buku::index');
-    $routes->get('buku/(:segment)', 'Buku::update/$1');
+    $routes->get('buku/(:segment)', 'Buku::show/$1');
     $routes->post('buku', 'Buku::create');
     $routes->put('buku/(:segment)', 'Buku::update/$1');
     $routes->delete('buku/(:segment)', 'Buku::delete/$1');
--- app/Modules/Buku/Controllers/Buku.php
+++ app/Modules/Buku/Controllers/Buku.php
@@ -10,72 +10,82 @@ class Buku extends BaseController
 {
     use ResponseTrait;
     protected $folder_directory = "Modules\\Buku\\Views\\";
-    protected $model;
 
     public function __construct()
     {
         // Load PegawaiModel untuk digunakan dalam method controller
         $this->model = new BukuModel;
     }
+    public function show($id_buku = null)
+    {
+        $data = [
+            'pesan' => 'Berhasil Mendapatkan Data Buku',
+            'data_buku' => $this->model->find($id_buku)
+        ];
+        if ($data['data_buku'] == null) {
+            return $this->failNotFound('Data Buku Tidak ditemukan');
+        }
+        return $this->respond($data, 200);
+    }
     public function index()
     {
         // $authHeader = $this->request->getHeader('Authorization');
         // if ($authHeader && $authHeader->getValue() === $this->value) {
-
+            
         // }else {
         //     return $this->failUnauthorized('Anda Tidak Memiliki Kunci Akses');
         // }
         $model = new BukuModel();
-        $data = $model->getBuku();
-        if ($this->request->isAJAX()) {
-            // Respond with JSON for AJAX requests (e.g., for your DataTables or API use)
-            return $this->respond(['buku' => $data]);
-        }
-        $Vdata = [
-            'buku' => $data,
-            'judul' => 'List Buku',
-        ];
-        return view($this->folder_directory . 'data_buku', $Vdata);
+            $data = $model->getBuku();
+            if ($this->request->isAJAX()) {
+                // Respond with JSON for AJAX requests (e.g., for your DataTables or API use)
+                return $this->respond(['buku' => $data]);
+            }
+            $Vdata = [
+                'buku' => $data,
+                'judul' => 'List Buku',
+            ];
+            return view($this->folder_directory . 'data_buku',$Vdata);
     }
     public function update($id_buku = null)
     {
         // $authHeader = $this->request->getHeader('Authorization');
         // // Mengecek apakah Authorization header valid
         // if ($authHeader && $authHeader->getValue() === $this->value) {
-
+            
         // } else {
         //     // Jika Authorization header tidak valid
         //     return $this->failUnauthorized('Anda Tidak Memiliki Kunci Akses');
         // }
-        // Ambil rules validasi dari model
-        $rules = $this->model->validationRules();
+            // Ambil rules validasi dari model
+            $rules = $this->model->validationRules();
+            
+            // Validasi input
+            if (!$this->validate($rules)) {
+                $response = [
+                    'pesan' => $this->validator->getErrors()
+                ];
+                return $this->failValidationErrors($response);
+            }
+            $Data = $this->model->find($id_buku);
+            if (!$Data) {
+                $response = [
+                    'Pesan' => 'Data Pegawai dengan ID tersebut tidak ditemukan'
+                ];
+                return $this->failNotFound('Data Pegawai dengan ID tersebut tidak ditemukan');
+            }
+            $this->model->update($id_buku, [
+                'kode_buku' => esc($this->request->getVar('kode_buku')),
+                'judul_buku' => esc($this->request->getVar('judul_buku')),
+                'pengarang' => esc($this->request->getVar('pengarang')),
+                'target_terbit' => esc($this->request->getVar('target_terbit')),
+                'warna' => esc($this->request->getVar('target_terbit')),
+            ]);
 
-        // Validasi input
-        if (!$this->validate($rules)) {
             $response = [
-                'pesan' => $this->validator->getErrors()
+                'Pesan' => 'Data Pegawai Berhasil dirubah'
             ];
-            return $this->failValidationErrors($response);
-        }
-        $Data = $this->model->find($id_buku);
-        if (!$Data) {
-            $response = [
-                'Pesan' => 'Data Pegawai dengan ID tersebut tidak ditemukan'
-            ];
-            return $this->failNotFound('Data Pegawai dengan ID tersebut tidak ditemukan');
-        }
-        $this->model->update($id_buku, [
-            'kode_buku' => esc($this->request->getVar('kode_buku')),
-            'judul_buku' => esc($this->request->getVar('judul_buku')),
-            'pengarang' => esc($this->request->getVar('pengarang')),
-            'target_terbit' => esc($this->request->getVar('target_terbit')),
-            'warna' => esc($this->request->getVar('target_terbit')),
-        ]);
-
-        $response = [
-            'Pesan' => 'Data Pegawai Berhasil dirubah'
-        ];
-        return $this->respond($response);
+            return $this->respond($response);
     }
     public function delete($id_buku = null)
     {
@@ -86,16 +96,15 @@ class Buku extends BaseController
         //     // Jika Authorization header tidak valid
         //     return $this->failUnauthorized('Anda Tidak Memiliki Kunci Akses');
         // }
-        $id_buku = base64_decode($id_buku);
-        $buku = $this->model->find($id_buku);
-        if (!$buku) {
-            return $this->failNotFound('Data Buku tidak ditemukan');
-        }
-        $this->model->delete($id_buku);
-        $response = [
-            'pesan' => 'Data Buku berhasil dihapus'
-        ];
-        return $this->respondDeleted($response, 200);
+            $buku = $this->model->find($id_buku);
+            if (!$buku) {
+                return $this->failNotFound('Data Buku tidak ditemukan');
+            }
+            $this->model->delete($id_buku);
+            $response = [
+                'pesan' => 'Data Buku berhasil dihapus'
+            ];
+            return $this->respondDeleted($response,200);
     }
     public function data_buku()
     {
