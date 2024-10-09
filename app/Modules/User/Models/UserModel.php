84f650c5f9eb1eb9e4b38abcded8ffda385c5a4d
Reinharrd <83526674+Reinharrd@users.noreply.github.com>
2024-10-09 11:08:47 +0700

    user

--- app/Modules/User/Models/UserModel.php
+++ app/Modules/User/Models/UserModel.php
@@ -5,7 +5,40 @@ namespace Modules\User\Models;
 use CodeIgniter\Model;
 
 class UserModel extends Model
-{
-    protected $table            = 'tbl_user';
-    protected $allowedFields    = [];
+{protected $table            = 'tbl_user';
+    protected $primaryKey       = 'id_user';
+    protected $useAutoIncrement = true;
+    protected $allowedFields    = ['nama', 'nomor_induk', 'email','no_tlp','jk','password','avatar','verifikasi','status_user','level_user' ];
+
+    // protected bool $allowEmptyInserts = false;
+    // protected bool $updateOnlyChanged = true;
+
+    // protected array $casts = [];
+    // protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+
+    // Fungsi untuk validasi data sebelum insert
+    public function validationRules()
+    {
+        return [
+            'nama' => 'required|min_length[3]',
+            'nomor_induk' => 'required|is_unique[tbl_user.nomor_induk]', 
+            'email' => 'required|valid_email|is_unique[tbl_user.email]',
+            'no_tlp' => 'required|numeric',
+            'jk' => 'required|in_list[Laki-Laki,Perempuan]',
+            'password' => 'required|min_length[6]',
+            'level_user' => 'required',
+        ];
+    }
+
+    // (Opsional) Hash password sebelum disimpan
+    public function getPegawai()
+    {
+        return $this->findAll();
+    }
 }
--- app/Modules/User/Views/data_user.php
+++ app/Modules/User/Views/data_user.php
@@ -37,7 +37,7 @@
                                         <i class="bx bx-dots-horizontal-rounded"></i>
                                     </button>
                                     <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-file me-2"></i> Detail</a>
+                                        <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-file me-2"></i> Verfikasi</a>
                                         <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-2"></i> Edit</a>
                                         <a class="dropdown-item" style="color: red;" href="javascript:void(0);"><i class="bx bx-trash me-2"></i> Delete</a>
                                     </div>
