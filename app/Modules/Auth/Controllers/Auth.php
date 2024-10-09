17cad2678d112605c7890815c3628ad24abfddcf
Hersa-Riantama <hersarb28@gmail.com>
2024-10-09 11:13:12 +0700

    Update

--- app/Modules/Auth/Controllers/Auth.php
+++ app/Modules/Auth/Controllers/Auth.php
@@ -3,10 +3,12 @@
 namespace Modules\Auth\Controllers;
 
 use App\Controllers\BaseController;
+use CodeIgniter\API\ResponseTrait;
 use Modules\Auth\Models\AuthModel;
 
 class Auth extends BaseController
 {
+    use ResponseTrait;
     protected $folder_directory = "Modules\\Auth\\Views\\";
     protected $model;
 
@@ -109,7 +111,7 @@ class Auth extends BaseController
         return $this->response->setJSON($response)->setStatusCode(200);
         // // Ambil nilai Authorization header
         // $authHeader = $this->request->getHeader('Authorization');
-        
+
         // // Cek apakah header Authorization ada dan nilai key cocok
         // if ($authHeader && $authHeader->getValue() === $this->value) {
         // } else {
