14041f2e7d69a644f00ade3af2c6a5caf0ba372c
Hersa-Riantama <hersarb28@gmail.com>
2024-10-10 14:36:10 +0700

    Update admin_template.php

--- app/Views/template/admin_template.php
+++ app/Views/template/admin_template.php
@@ -202,18 +202,17 @@
             <!-- Layout container -->
             <div class="layout-page">
                 <!-- Navbar -->
-                <nav
-                    class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
-                    id="layout-navbar">
+                <!-- <nav
+                    class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
                     <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
                         <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
                             <i class="bx bx-menu bx-sm"></i>
                         </a>
                     </div>
 
-                    <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-                        <!-- Search -->
-                        <div class="navbar-nav align-items-center">
+                    <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse"> -->
+                <!-- Search -->
+                <!-- <div class="navbar-nav align-items-center">
                             <div class="nav-item d-flex align-items-center">
                                 <i class="bx bx-search fs-4 lh-0"></i>
                                 <input
@@ -222,12 +221,12 @@
                                     placeholder="Search..."
                                     aria-label="Search..." />
                             </div>
-                        </div>
-                        <!-- /Search -->
+                        </div> -->
+                <!-- /Search -->
 
-                        <ul class="navbar-nav flex-row align-items-center ms-auto">
-                            <!-- Place this tag where you want the button to render. -->
-                            <!-- <li class="nav-item lh-1 me-3">
+                <!-- <ul class="navbar-nav flex-row align-items-center ms-auto"> -->
+                <!-- Place this tag where you want the button to render. -->
+                <!-- <li class="nav-item lh-1 me-3">
                                 <a
                                     class="github-button"
                                     href="https://github.com/themeselection/sneat-html-admin-template-free"
@@ -237,8 +236,8 @@
                                     aria-label="Star themeselection/sneat-html-admin-template-free on GitHub">Star</a>
                             </li> -->
 
-                            <!-- User -->
-                            <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                <!-- User -->
+                <!-- <li class="nav-item navbar-dropdown dropdown-user dropdown">
                                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                                     <div class="avatar avatar-online">
                                         <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
@@ -275,7 +274,7 @@
                                             <span class="align-middle">Settings</span>
                                         </a>
                                     </li>
-                                    <!-- <li>
+                                    <li>
                                         <a class="dropdown-item" href="#">
                                             <span class="d-flex align-items-center align-middle">
                                                 <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
@@ -283,7 +282,7 @@
                                                 <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
                                             </span>
                                         </a>
-                                    </li> -->
+                                    </li>
                                     <li>
                                         <div class="dropdown-divider"></div>
                                     </li>
@@ -294,11 +293,11 @@
                                         </a>
                                     </li>
                                 </ul>
-                            </li>
-                            <!--/ User -->
-                        </ul>
-                    </div>
-                </nav>
+                            </li> -->
+                <!--/ User -->
+                <!-- </ul> -->
+                <!-- </div> -->
+                <!-- </nav> -->
                 <!-- / Navbar -->
 
                 <?= $this->renderSection('content'); ?>
