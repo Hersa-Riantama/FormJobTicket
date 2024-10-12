11f0c8a5a88bd12694c08b4f168b7bccf0d856ff
Hersa-Riantama <hersarb28@gmail.com>
2024-10-12 12:29:52 +0700

    Update form.php

--- app/Modules/Form/Views/form.php
+++ app/Modules/Form/Views/form.php
@@ -430,7 +430,7 @@
 
                 <div class="container-xxl flex-grow-1" style="padding-bottom: 0.25rem">
                     <!-- Row 3 -->
-                    <div class="row justify-content-center mb-3 mt-2">
+                    <div class="row justify-content-center my-3">
                         <div class="col-xl-6">
                             <button class="btn btn-primary d-grid w-100" id="btnsimpan">Simpan</button>
                         </div>
