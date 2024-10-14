1bd7e80d8392dfeaf04e2f80416446803534c4d8
Hersa-Riantama <hersarb28@gmail.com>
2024-10-14 11:48:32 +0700

    Update Routes.php

--- app/Modules/User/Config/Routes.php
+++ app/Modules/User/Config/Routes.php
@@ -18,7 +18,7 @@ $routes->group(
 );
 
 $routes->group(
-    '',
+    'api',
     ['namespace' => '\Modules\User\Controllers'],
     function ($routes) {
         $routes->get('user', 'User::tampil');
