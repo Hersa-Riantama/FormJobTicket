9b689f71349435e3678704e6a78da24d45b49af9
Reinharrd <83526674+Reinharrd@users.noreply.github.com>
2024-10-17 13:55:38 +0700

    rbac

--- app/Modules/Buku/Controllers/Buku.php
+++ app/Modules/Buku/Controllers/Buku.php
@@ -65,6 +65,26 @@ class Buku extends BaseController
         //     // Jika header Authorization tidak valid
         //     return $this->failUnauthorized('Anda Tidak Memiliki Kunci Akses');
         // }
+        $AuthModel = new AuthModel();
+        // Ambil data user berdasarkan ID dari sesi
+        $userId = session()->get('id_user');
+        if (!empty($userId)) {
+            // Ambil data user dari database berdasarkan id_user
+            $userData = $AuthModel->find($userId);
+            if ($userData && isset($userData['level_user'])) {
+                $allowUser = ['Editor','Koord Editor'];
+                if (!in_array($userData['level_user'],$allowUser)) {
+                    echo '<script>alert("Access Denied!!"); history.back();</script>';
+                    return;
+                }
+            } else {
+                echo '<script>alert("Level user tidak ditemukan."); history.back();</script>';
+                return;
+            }
+        } else {
+            echo '<script>alert("User not found or session invalid."); history.back();</script>';
+            return;
+        }
         $rules = $this->model->validationRules();
         if (!$this->validate($rules)) {
             $response = [
@@ -137,7 +157,23 @@ class Buku extends BaseController
         $AuthModel = new AuthModel();
         // Ambil data user berdasarkan ID dari sesi
         $userId = session()->get('id_user');
-        $userData = $AuthModel->find($userId);
+        if (!empty($userId)) {
+            // Ambil data user dari database berdasarkan id_user
+            $userData = $AuthModel->find($userId);
+            if ($userData && isset($userData['level_user'])) {
+                $allowUser = ['Editor','Admin Sistem','Koord Editor'];
+                if (!in_array($userData['level_user'],$allowUser)) {
+                    echo '<script>alert("Access Denied!!"); history.back();</script>';
+                    return;
+                }
+            } else {
+                echo '<script>alert("Level user tidak ditemukan."); history.back();</script>';
+                return;
+            }
+        } else {
+            echo '<script>alert("User not found or session invalid."); history.back();</script>';
+            return;
+        }
         $data = [
             'judul' => 'Kelola Buku',
             'userData' => $userData,
