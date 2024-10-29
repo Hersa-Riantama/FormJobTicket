39e9031889ce23e46ddfcb6548c26069088178b6
Hersa-Riantama <hersarb28@gmail.com>
2024-10-29 15:02:49 +0700

    update

--- app/Modules/Form/Views/detailForm.php
+++ app/Modules/Form/Views/detailForm.php
@@ -9,6 +9,9 @@
             <div class="container-xxl flex-grow-1" style="padding-bottom: 0.25rem">
                 <h4 class="py-3 mb-4"><span class="text-muted fw-light">Form /</span> Form QR Code</h4>
                 <div class="row justify-content-between align-items-start">
+                    <h6 class="mb-0 text-hitam" style="font-weight: 100;">FRM.DGT.05.01/</h6>
+                </div>
+                <div class="row justify-content-between align-items-start">
                     <div class="col-xl-4 mb-5 d-flex justify-content-center">
                         <img src="<?= base_url('/assets/img/icons/Form QR Code.jpg') ?>"
                             style="width: 100%; max-width: 100%; max-height: 14rem; object-fit: cover;"
