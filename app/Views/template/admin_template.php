b1dfe5c7ce2025b34087fb52c51ad630d5cb9c1c
Hersa-Riantama <hersarb28@gmail.com>
2024-11-06 15:00:23 +0700

    Update admin_template.php

--- app/Views/template/admin_template.php
+++ app/Views/template/admin_template.php
@@ -28,7 +28,6 @@
     <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
     <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="../assets/css/demo.css" />
-    <link rel="stylesheet" href="../assets/css/core.css" />
 
     <!-- Vendors CSS -->
     <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
